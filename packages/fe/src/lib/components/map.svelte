ad5ba96f7553e9b49d08f4eff31df3dd3e4de970
Eisen Montalvo <eisen@mac.com>
2022-10-30 12:03:44 -0600

    Got world map rendering

---
+++ packages/fe/src/lib/components/map.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { geoGraticule } from 'd3-geo'
+
+	export let data;
+
+	const graticuleGen = geoGraticule();
+</script>
+
+<svg>
+	<g id="graticules" />
+	<g id="countries" />
+</svg>
--- packages/fe/src/routes/+page.svelte
+++ packages/fe/src/routes/+page.svelte
@@ -1,3 +1,41 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<script lang="ts">
+  import { geoGraticule, geoMercator, geoPath } from 'd3-geo'
+  import { json } from 'd3-fetch'
+  import { feature } from 'topojson'
+  import { onMount } from 'svelte'
+
+  let data: any
+  $: data = null
+
+  const graticuleGen = geoGraticule()
+  const graticuleUle = graticuleGen.lines()
+  const graticuleOutline = graticuleGen.outline()
+
+  const projection = geoMercator().scale(150)
+  const path = geoPath().projection(projection)
+
+  onMount(async () => {
+    const features: any = await json('http://localhost:5173/data/world.json')
+    data = feature(features, features.objects.countries)
+  })
+</script>
+
+<div class="absolute inset-0">
+  <svg id="map" width="100%" height="100%">
+    <g id="graticules">
+      {#each graticuleUle as line}
+        <path d={path(line)} fill="none" stroke="lightgray" />
+      {/each}
+      <g id="outline">
+        <path d={path(graticuleOutline)} fill="none" stroke="black" stroke-width="2" />
+      </g>
+    </g>
+    <g id="countries">
+      {#if data}
+        {#each data.features as feature}
+          <path id={feature.id} d={path(feature)} stroke="lightgray" fill="none" />
+        {/each}
+      {/if}
+    </g>
+  </svg>
+</div>
