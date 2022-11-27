2365edfbe0e5235b27a4896a8fb215ccbd135f04
Nishita <u1320454@utah.edu>
2022-11-27 07:42:45 -0700

    Implemented individual area chart + Optimised area chart code + Added axes

--- src/lib/components/area.svelte
+++ src/lib/components/area.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-  import { Types } from "$lib/utilities"
+  import { Types, Helpers } from "$lib/utilities"
   import type { ArtistData, ArtistInfluence, ArtistLocation, ArtistMedium } from "$lib/utilities/types"
   import * as d3 from "d3"
-    import { geoConicConformal } from "d3";
 
   export let width: number = 0
   export let height: number = 0
@@ -28,53 +27,145 @@
   $: year = 0
   const tickEvery = 20
   const yearEvery = 100
+  const yearEverySingle = 10
 
   let tl_x_scale: d3.ScaleLinear<number, number, never>
+  $: tl_x_scale
   let tl_x_axis = null
+  let tl_y_scale: d3.ScaleLinear<number, number, never>
+  $: tl_y_scale
+  let tl_y_axis = null
 
   let size_years: number | undefined
   let years = new Array<number>(size_years!);
 
   let group_artists: any = []
+  let artists_alive: any = []
   let num_categories = new Array<number>(5)
-  // $: num_categories 
+  $: num_categories = []
 
+  let area_lines:number[][]
+  $: area_lines = []
 
-  const calculate_percentage = (year: number) => {
-    // console.log('year', year)
-    // group_artists = d3.groups(artists, d => d.artist)
-    // console.log('group_artists', group_artists)
+  let single_total:number
+  // $: single_total
+
+  let category_names = ['painter', 'watercolourist', 'oilpainter', 'sculptor', 'printmaker', 'draughtsman', 'muralist']//,
+//'photography', 'film', 'illustrator', 'architect', 'ink', 'ceramicist', 'calligrapher', 'engraving', 'calligrapher']
+
+  // let chordColorScale = d3
+  //   .scaleOrdinal()
+  //   .domain(d3.range(Helpers.ColorScheme.length))
+  //   .range(Helpers.ColorScheme)
+
+  let chordColorScale = d3
+    .scaleOrdinal()
+    .domain(d3.range(10))
+    .range(d3.schemeCategory10)
+
+  const DataForAreaChartLine = (category:string) => {
+    let area_values = new Array<number>(size_years!)
+
+    let i=0
+    for(var year of years){
+      // console.log(i)
+      let total_mediums_one_year = 0
+      let alive = AliveArtists(year)
+      // console.log('alive', alive)
+      area_values[i] = 0
+      let alive_artist_medium_list:any = []
+      for(var alive_artist of alive){
+        // console.log(alive_artist)
+        alive_artist_medium_list = mediums.filter(d => d.artist===alive_artist[0])
+        for(var medium of alive_artist_medium_list){
+          if(medium.medium === category){
+            area_values[i] = area_values[i] + 1
+          }
+          total_mediums_one_year = total_mediums_one_year + 1
+        }
+      }
+      // console.log(i, total_mediums_one_year)
+      single_total = single_total > total_mediums_one_year? single_total : total_mediums_one_year // Used for y axis 
+      // console.log(area_values[i])
+      // convert to percentage
+      area_values[i] = (area_values[i] / total_mediums_one_year) * 100
+      i=i+1
+    }
+    // console.log(area_values)
+    return area_values
+
+    // for(let i=0; i<years.length; i++){
+    //   area_values[i] = (area_values[i]/total) * chart_height
+    // }
     
-    const artists_alive = group_artists.filter(([, locations]) => {
+  }
+  
+  const IndividualAreaChart = (category:string) => {
+    let area_line_values = DataForAreaChartLine(category)
+    // console.log(area_line_values)
+
+    // X SCALE
+    var xScale = d3.scaleLinear()
+    .domain([0, size_years!])
+    .range([0, chart_width]);
+
+    // Y SCALE
+    // let maxY = d3.max(area_line_values!)
+    let maxY = single_total
+    var yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([chart_height, 0]);
+
+    
+    const areaGenerator = d3.area()
+      .x((d) => xScale(d.x - oldestYear!) + PADDING.left)
+      .y1(d => yScale(d.y) + PADDING.top)
+      // .x((d) => tl_x_scale(d.x) + PADDING.left)
+      // .y1(d => tl_y_scale(d.y) + PADDING.top)
+      .y0(chart_height + PADDING.top)
+      .curve(d3.curveCatmullRom.alpha(0.6))
+      .context(null);
+
+    for(let i=0; i<years.length; i++){
+      const each_pair: any = {
+        x: years[i],
+        y: area_line_values[i],
+      }
+      area_lines.push(each_pair)
+    }
+    
+    d3.select('#area-chart')
+    .datum(area_lines)
+    .attr("d", areaGenerator)
+    .style('fill', '#ee7722') // TODO: change this colour selection 
+    
+  }
+
+  const AliveArtists = (year: number) => {
+    let alive = group_artists.filter(([, locations]) => {
       if (locations.length > 1) {
         return locations[0].year <= year && locations[1].year > year
       } else {
         return locations[0].year <= year // if alive 
       }
     })
-    // console.log(artists_alive)
-    let category_mediums = [0,0,0,0,0]; // number of categories considered, [painter, sculptor, watercolourist, printmaker, draughtsman]
-    // console.log('arr', category_mediums[0])
+    return alive
+  }
+
+  const CalculatePercentage = (year: number) => {
+    
+    artists_alive = AliveArtists(year)
+
+    let category_mediums = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // number of categories considered, [painter, sculptor, watercolourist, printmaker, draughtsman]
+
     for(var alive_artist of artists_alive){
-      // console.log(alive_artist)
       const artist_medium_list = mediums.filter(d => d.artist===alive_artist[0])
-      // console.log(artist_medium_list)
       for(var medium of artist_medium_list){
-        // console.log(medium.medium)
-        if(medium.medium === 'painter'){
-          category_mediums[0] = category_mediums[0] + 1
-        }
-        if(medium.medium === 'watercolourist'){
-          category_mediums[1] = category_mediums[1] + 1
-        }
-        if(medium.medium === 'sculptor'){
-          category_mediums[2] = category_mediums[2] + 1
-        }
-        if(medium.medium === 'printmaker'){
-          category_mediums[3] = category_mediums[3] + 1
-        }
-        if(medium.medium === 'draughtsman'){
-          category_mediums[4] = category_mediums[4] + 1
+        for(var category of category_names){
+          if(medium.medium === category){
+            let index = category_names.indexOf(category)
+            category_mediums[index] = category_mediums[index] + 1
+          }
         }
       }
     }
@@ -86,7 +177,6 @@
       category_mediums[i] = total
     }
     // console.log('after category_mediums', category_mediums)
-
     for(let i=0; i<category_mediums.length; i++){
       category_mediums[i] = (category_mediums[i]/total) * chart_height
     }
@@ -94,41 +184,51 @@
 
     return category_mediums;
 
-    // return 100;
-    
-
-    // for a year
-    // for a medium 
-    // take all medium 
-
-
-    //chart from hw3
     //vertical line hover from hw4
 
   }
 
-  const colourFunc = (num: number) => {
-    // console.log('num', num)
-    if(num===0){
-      return "#ff0000"
+  const ColourFunc = (num: number):string => {
+    if(num){
+      return chordColorScale(num.toString()) as string
     }
-    if(num===1){
-      return "#00ff00"
-    }
-    if(num===2){
-      return "#0000ff"
-    }
-    if(num===3){
-      return "#00ffff"
-    }
-    if(num===4){
-      return "#ff00ff"
+    else{
+      return 'black'
     }
   }
 
-  const areaChart = () => {
-    console.log('here')
-    
+  const Stroke_size = () => {
+    let stroke_width = chart_width / size_years!
+    return stroke_width
+  }
+
+  const DrawAxesSingle = () => {
+    tl_x_scale = d3
+      .scaleLinear()
+      .domain([oldestYear!, youngestYear!])
+      .range([0, chart_width])
+    tl_x_axis = d3
+      .axisBottom(tl_x_scale)
+      .tickFormat((d) => {
+        return Number(d) % yearEverySingle === 0 ? String(d) : ""
+      })
+      .ticks((youngestYear! - oldestYear!) / tickEvery)
+    d3.select('#x-axis')
+    .attr('transform', `translate(${PADDING.left}, ${chart_height + PADDING.top})`)
+    .call(tl_x_axis)
+
+    // percentage 
+    tl_y_scale = d3
+      .scaleLinear()
+      // .domain([0, single_total!])
+      .domain([0, 100])
+      .range([chart_height, 0])
+    tl_y_axis = d3
+      .axisLeft(tl_y_scale)
+    d3.select('#y-axis')
+    .attr('transform', `translate(${PADDING.left}, ${PADDING.top})`)
+    .call(tl_y_axis)
+
   }
 
   export const Initialize = (
@@ -138,40 +238,27 @@
     artists = location_data
     mediums = medium_data
 
-    chart_width = height - PADDING.left - PADDING.right
-    chart_height = width - PADDING.top - PADDING.bottom
+    chart_width = width - PADDING.left - PADDING.right
+    chart_height = height - PADDING.top - PADDING.bottom
 
-    chart_height = 400
 
-    oldestYear = +d3.min(location_data, (d) => d.year)! // TODO: CHANGE THIS
-    oldestYear = 1700
-    year = oldestYear!
+    oldestYear = +d3.min(location_data, (d) => d.year)! // TODO: Change this
+    oldestYear = 1500
     youngestYear = +d3.max(location_data, (d) => d.year)!
-    size_years = youngestYear - oldestYear
+    youngestYear = 1600
+    size_years = youngestYear - oldestYear // TODO: change this when clicking on century button to show only that century
     for(let i=0; i<size_years; i++){
-      years[i] = oldestYear + i
+      years[i] = oldestYear + i 
     }
-    console.log('years', years)
-
-    // let all_years =  
-
-    tl_x_scale = d3
-        .scaleLinear()
-        .domain([oldestYear!, youngestYear!])
-        .range([0, width])
-    tl_x_axis = d3
-      .axisBottom(tl_x_scale)
-      .tickFormat((d) => {
-        return Number(d) % yearEvery === 0 ? String(d) : ""
-      })
-      .ticks((youngestYear! - oldestYear!) / tickEvery)
- 
-    areaChart()
 
     group_artists = d3.groups(artists, d => d.artist)
+    num_categories = d3.range(category_names.length).reverse()
+    // CalculatePercentage(1900)
+    // Stroke_size()
 
-    num_categories = [5, 4 , 3 , 2 , 1 ,0]
-    calculate_percentage(1900)
+    // TODO: onClick, selecting the medium should display only that category
+    IndividualAreaChart('painter')
+    DrawAxesSingle()
   }
 </script>
 
@@ -188,60 +275,30 @@
     >
   <!-- #each for all years, call function for each year calc mediums % , #each give line generater -->
   <g id="lines-chart">
-    <!-- {#each category_mediums as category, category_num} -->
+    <g id="x-axis"></g>
+    <g id="y-axis"></g>
+    <g id="single-area">
+      <path id="area-chart"></path>
+    </g>
+    
+    <!-- <g id="all-area">
       {#each years as year, idx}
-        <!-- {#each num_categories as num}
-          <g/>
-        {/each} -->
-        <g>
-          <line
-            x1={idx}
-            y1={PADDING.top + chart_height - (calculate_percentage ? calculate_percentage(+year)[4] : 200)}
-            x2={idx}
+        {#each num_categories as num}
+          <g>
+            <line
+            x1={PADDING.left + idx*Stroke_size()}
+            y1={PADDING.top + chart_height - (CalculatePercentage ? CalculatePercentage(+year)[num] : 200)}
+            x2={PADDING.left + idx*Stroke_size()}
             y2={PADDING.top + chart_height}
-            stroke-width="1.0"
-            stroke={colourFunc(4)}
-            opacity="0.2"
-          />
-          <line
-            x1={idx}
-            y1={PADDING.top + chart_height - (calculate_percentage ? calculate_percentage(+year)[3] : 200)}
-            x2={idx}
-            y2={PADDING.top + chart_height}
-            stroke-width="1.0"
-            stroke={colourFunc(3)}
-            opacity="0.2"
-          />
-          <line
-            x1={idx}
-            y1={PADDING.top + chart_height - (calculate_percentage ? calculate_percentage(+year)[2] : 200)}
-            x2={idx}
-            y2={PADDING.top + chart_height}
-            stroke-width="1.0"
-            stroke={colourFunc(2)}
-            opacity="0.2"
-          />
-          <line
-            x1={idx}
-            y1={PADDING.top + chart_height - (calculate_percentage ? calculate_percentage(+year)[1] : 200)}
-            x2={idx}
-            y2={PADDING.top + chart_height}
-            stroke-width="1.0"
-            stroke={colourFunc(1)}
-            opacity="0.2"
-          />
-          <line
-            x1={idx}
-            y1={PADDING.top + chart_height - (calculate_percentage ? calculate_percentage(+year)[0] : 200)}
-            x2={idx}
-            y2={PADDING.top + chart_height}
-            stroke-width="1.0"
-            stroke={colourFunc(0)}
-            opacity="0.2"
-          />
-        </g>
+            stroke-width={Stroke_size()}
+            stroke={ColourFunc(num)}
+            opacity="1.0"
+            />
+          </g>
+        {/each}
       {/each}
-    <!-- {/each} -->
+    </g> -->
+      
   </g>
   <!-- each, each, out, line, out -->
 <!-- vertical lines -->
